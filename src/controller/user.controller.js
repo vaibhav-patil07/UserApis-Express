77f6357c5e8fbfb1e66e7ea858f8f61e0c1f9724
Vaibhav Patil <vaibhav.patil@contentstack.com>
2023-02-10 11:33:51 +0530

    Added comments for user controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -8,6 +8,7 @@ let latestUserId = 13;
 const UserFilePath = "./data/Users.json";
 
 const getAllUsers = (req, res) => {
+  //Promise for reading file
   FileHandler.readFileAsync(UserFilePath, "utf-8")
     .then((data) => {
       sendResponse(req, res, {
@@ -21,12 +22,16 @@ const getAllUsers = (req, res) => {
 };
 
 const getUserById = (req, res) => {
+  //get user id from params
   const {
     params: { id },
   } = req;
+
+  //Promise for reading file
   FileHandler.readFileAsync(UserFilePath, "utf-8")
     .then((data) => {
       const result = data.find((user) => user.id === id);
+      //If user not found
       if (!result) {
         return sendError(
           new AppError({ message: "User not found", statusCode: 404 }),
@@ -34,12 +39,14 @@ const getUserById = (req, res) => {
           res
         );
       }
+      //If user data found
       return sendResponse(req, res, {
         message: "User fetched successfully",
         payload: result,
       });
     })
     .catch((err) => {
+      //Error handling for reading file
       sendError(new AppError({ message: err, statusCode: 500 }), req, res);
     });
 };
@@ -49,6 +56,7 @@ const validateUser = (req, res, next) => {
   const isValid = User.prototype.keys.every((keys) =>
     Object.keys(body).includes(keys)
   );
+  //If request body is invalid
   if (!isValid) {
     return sendError(
       new AppError({ message: "Invalid request body", statusCode: 400 }),
@@ -60,19 +68,28 @@ const validateUser = (req, res, next) => {
 };
 
 const createUser = (req, res) => {
+  //validateUser -> next -> createUser
+
   const { body } = req;
   const userData = new User(body);
+
+  //Increament user id
   latestUserId++;
-  userData.id = latestUserId.toString();
+  userData.id = latestUserId.toString(); //Assign id
+
+  //Read promise
   const readPromise = FileHandler.readFileAsync(UserFilePath, "utf-8");
 
+  //Write Promise
   const writePromise = readPromise
     .then((data) => {
       const Users = [...data];
       Users.push(userData);
+      //After successful read return write promise
       return FileHandler.writeFileAsync(UserFilePath, "utf-8", Users);
     })
     .catch((err) => {
+      //Error handling for read operation
       return sendError(
         new AppError({ message: err, statusCode: 500 }),
         req,
@@ -82,11 +99,13 @@ const createUser = (req, res) => {
 
   writePromise
     .then((data) => {
+      //After successful write send response
       return sendResponse(req, res, {
         message: "User created successfully",
         payload: userData,
       });
     })
+    //Error handling for write operation
     .catch((err) => {
       return sendError(
         new AppError({ message: err, statusCode: 500 }),
@@ -100,25 +119,33 @@ const deleteUser = (req, res) => {
   const {
     params: { id },
   } = req;
+  //Read promise
   FileHandler.readFileAsync(UserFilePath, "utf-8")
     .then((data) => {
+      //Check if user with id exists
       const result = data.findIndex((user) => user.id === id);
       if (result === -1) {
+        //If user not found
         return sendError(
           new AppError({ message: "User not found", statusCode: 404 }),
           req,
           res
         );
       }
+
+      //If user found for id delete and write
       const deletedUser = data.splice(result, 1);
+
       FileHandler.writeFileAsync(UserFilePath, "utf-8", data)
         .then((data) => {
+          //Response for deleted user
           return sendResponse(req, res, {
             message: "User deleted successfully",
             payload: deletedUser,
           });
         })
         .catch((err) => {
+          //Error handling for write
           return sendError(
             new AppError({ message: err, statusCode: 500 }),
             req,
@@ -127,6 +154,7 @@ const deleteUser = (req, res) => {
         });
     })
     .catch((err) => {
+      //Error handling for read
       sendError(new AppError({ message: err, statusCode: 500 }), req, res);
     });
 };
@@ -140,31 +168,40 @@ const updateUser = (req, res) => {
   const userData = new User(body);
   userData.id = id;
 
+  //write promise
   const writePromise = FileHandler.readFileAsync(UserFilePath, "utf-8")
     .then((data) => {
       let result = data.find((user) => user.id === id);
       if (!result) {
+        //User not found for id
         return sendError(
           new AppError({ message: "User not found", statusCode: 404 }),
           req,
           res
         );
       }
+
+      //if user found update its data
       result = userData;
+
+      //return write promise
       return FileHandler.writeFileAsync(UserFilePath, "utf-8", data);
     })
     .catch((err) => {
+      //Error handling for read operation
       sendError(new AppError({ message: err, statusCode: 500 }), req, res);
     });
 
   writePromise
     .then((data) => {
+      //After successful write return updated user data
       return sendResponse(req, res, {
         message: "User updated successfully",
         payload: userData,
       });
     })
     .catch((err) => {
+      //Error handling for write operation
       return sendError(
         new AppError({ message: err, statusCode: 500 }),
         req,
